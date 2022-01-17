d54d6337d2bf2bab350bf28f08d6e75d63210d65
Cristian Felipe Vargas Hernandez <cfvargas@bancolombia.com.co>
2022-01-16 22:03:17 -0500

    Added example multiple environments

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -6,7 +6,7 @@ export default function navigation() {
     return (
         <nav className="nav" role="navigation">
             <div className="max-centered">
-                <h1 className="nav__logo">Rule of thumb.</h1>
+                <h1 className="nav__logo">{process.env.REACT_APP_WEBSITE_NAME}</h1>
                 <button className="nav__hamburger icon-button" alt="Open Menu">
                     <svg width="25" height="20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M0 0h25v4H0V0zm0 8h25v4H0V8zm0 8h25v4H0v-4z" fill="#FFF" fillRule="nonzero" />
--- src/constants/sizes.js
+++ src/constants/sizes.js
@@ -1,7 +1,5 @@
 const sizes = {
     MAX_MOBILE: 786,
-    TABLET: 900,
-    DESKTOP: 1100
 }
 
 
